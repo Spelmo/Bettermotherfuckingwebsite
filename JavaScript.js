317c6c54f274cb3c7f29ccf1acd3c61004233bc5
Spelmo <42944289+Spelmo@users.noreply.github.com>
2018-09-27 19:54:46 +0200

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,9 @@
+setTimeout(function(){
+  if (confirm("Hey le site xxxvidsxxx est trop bien. Veux-tu aller sur le site?"))
+   window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ/")
+} ,2000);
+
+
+function psychedelic(){
+  document.body.style.backgroundColor='magenta';
+}
