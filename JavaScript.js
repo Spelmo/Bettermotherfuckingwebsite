4e552b05af8c11616b4c548ece943f5444486bb3
Spelmo <42944289+Spelmo@users.noreply.github.com>
2018-09-27 19:09:51 +0200

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -3,7 +3,7 @@ document.onload = setTimeout(Popup(), 10000);
 function Popup(){
   var r = window.confirm("Hey le site xxxvidsxxx est trop bien. Veux-tu aller sur le site?");
   if (r=true) {
-  then  window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ/");
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ/");
   else event.cancel
   }
 }
