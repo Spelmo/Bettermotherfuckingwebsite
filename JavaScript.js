93ef899b834d28c353fb2311562b6290dce4b1f8
Spelmo <42944289+Spelmo@users.noreply.github.com>
2018-09-27 19:15:15 +0200

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,12 +1,10 @@
-document.onload = setTimeout(Popup(), 1000);
-
 function Popup(){
-  var r = window.confirm("Hey le site xxxvidsxxx est trop bien. Veux-tu aller sur le site?");
-  if (r=true) {
+  if (window.confirm("Hey le site xxxvidsxxx est trop bien. Veux-tu aller sur le site?")); {
     window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ/");
   }
 }
 
+document.onload = setTimeout(Popup(), 1000);
 
 function psychedelic(){
   document.body.style.backgroundColor='magenta';
