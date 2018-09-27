3b0c85f053b96aa8e0380530ea4544a6d4367f72
Spelmo <42944289+Spelmo@users.noreply.github.com>
2018-09-27 20:02:25 +0200

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,9 +1,8 @@
 document.onload = setTimeout(function(){
   if (window.confirm("Hey le site xxxvidsxxx est trop bien. Veux-tu aller sur le site?")) {
     window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ/");
-   else null;
   }
-} ,2000);
+} ,10000);
 
 
 function psychedelic(){
